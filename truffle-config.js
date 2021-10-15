8ce62ff74c80bfdaa60f70b23a8fddaa01eb998f
Kelvin_thai <quan.thai1987@gmail.com>
2021-10-15 11:05:34 +0700

    first commit

---
+++ truffle-config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const HDWalletProvider = require("truffle-hdwallet-provider")
+const fs = require('fs');
+
+
+
+const infuraKey = "367af6bb579f43cebfdf20ba85148f38";
+
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  networks: {
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraKey}`),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true,     // Skip dry run before migrations? (default: false for public nets )
+      }
+  }
+};
